09a5f8fe253cf775805cad1bf359a2afcb2514cd
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2024-05-12 13:04:55 -0300

    feat Tela Criada

--- Remediar-FrontEnd/src/app/app.routes.ts
+++ Remediar-FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HistoricoDoadoresComponent } from './historico-doadores/historico-doadores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    component: HistoricoDoadoresComponent
+}];
---
+++ Remediar-FrontEnd/src/app/historico-doadores/historico-doadores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historico-doadores',
+  templateUrl: './historico-doadores.component.html',
+  styleUrls: ['./historico-doadores.component.css']
+})
+export class HistoricoDoadoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
