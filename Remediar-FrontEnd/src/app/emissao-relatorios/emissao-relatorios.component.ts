2fc75bbca76fde404d8526b9473966300d351827
ThaisAlvesSilva <alvesthais406@gmail.com>
2024-05-20 19:50:17 -0300

    Finalizando front da tela

--- Remediar-FrontEnd/src/app/emissao-relatorios/emissao-relatorios.component.ts
+++ Remediar-FrontEnd/src/app/emissao-relatorios/emissao-relatorios.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-emissao-relatorios',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './emissao-relatorios.component.html',
   styleUrl: './emissao-relatorios.component.css'
 })
 export class EmissaoRelatoriosComponent {
 
+  constructor(private router: Router) {}
+
+    goBack(): void {
+      this.router.navigate(['/']); // Navega para a rota anterior
+    }
 }
