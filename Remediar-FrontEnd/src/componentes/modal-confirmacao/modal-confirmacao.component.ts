414251b99814ff7627dbceeeb9cb16a167c8a774
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2024-06-01 22:04:47 -0300

    Criando componentes

---
+++ Remediar-FrontEnd/src/componentes/modal-confirmacao/modal-confirmacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirmacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-confirmacao.component.html',
+  styleUrl: './modal-confirmacao.component.css'
+})
+export class ModalConfirmacaoComponent {
+
+}
---
+++ Remediar-FrontEnd/src/componentes/tabela-estoque/tabela-estoque.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-estoque',
+  standalone: true,
+  imports: [],
+  templateUrl: './tabela-estoque.component.html',
+  styleUrl: './tabela-estoque.component.css'
+})
+export class TabelaEstoqueComponent {
+
+}
