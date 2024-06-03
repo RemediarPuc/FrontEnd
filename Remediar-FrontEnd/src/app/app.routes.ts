d1f3a54ed0595106eb9af39093e38b83ebb0798e
gabriellaxdantas <gabriellaxdantas@gmail.com>
2024-06-03 20:59:21 -0300

    tela inicial do adm

--- Remediar-FrontEnd/src/app/app.routes.ts
+++ Remediar-FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { TelaInicialAdmComponent } from './tela-inicial-adm/tela-inicial-adm.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+ {
+    path: '',
+    component: TelaInicialAdmComponent
+ }
+
+];
---
+++ Remediar-FrontEnd/src/app/tela-inicial-adm/tela-inicial-adm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicial-adm',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-inicial-adm.component.html',
+  styleUrl: './tela-inicial-adm.component.css'
+})
+export class TelaInicialAdmComponent {
+
+}
