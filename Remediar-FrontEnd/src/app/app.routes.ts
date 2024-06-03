1f0f40f16d9d5920973a560cd56f3950243a8e4b
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2024-06-02 23:46:04 -0300

    Criando estrutura tela de estoque

--- Remediar-FrontEnd/src/app/app.routes.ts
+++ Remediar-FrontEnd/src/app/app.routes.ts
@@ -1,6 +1,6 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { TabelaEstoqueComponent } from './componentes/tabela-estoque/tabela-estoque.component';
-import { ModalConfirmacaoComponent } from './componentes/modal-confirmacao/modal-confirmacao.component';
 
 export const routes: Routes = [
     {
