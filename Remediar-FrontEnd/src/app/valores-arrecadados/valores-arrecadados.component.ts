587d9768800c866731203255d0bd0f010ab431ff
gabriellaxdantas <gabriellaxdantas@gmail.com>
2024-06-04 19:08:09 -0300

    componente valores arrecadados

---
+++ Remediar-FrontEnd/src/app/valores-arrecadados/valores-arrecadados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valores-arrecadados',
+  standalone: true,
+  imports: [],
+  templateUrl: './valores-arrecadados.component.html',
+  styleUrl: './valores-arrecadados.component.css'
+})
+export class ValoresArrecadadosComponent {
+
+}
