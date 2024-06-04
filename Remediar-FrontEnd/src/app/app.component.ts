e1f73d160e91176a9dd067e910e962f54eac60c0
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2024-06-04 10:17:29 -0300

    Removendo import

--- Remediar-FrontEnd/src/app/app.component.ts
+++ Remediar-FrontEnd/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TabelaEstoqueComponent } from './componentes/tabela-estoque/tabela-estoque.component';
-import { ModalConfirmacaoComponent } from './componentes/modal-confirmacao/modal-confirmacao.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, TabelaEstoqueComponent, ModalConfirmacaoComponent],
+  imports: [RouterOutlet, RouterLink, TabelaEstoqueComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
