ffd8b6d4b389662667247b1a4c3b6ca1fdb7d919
ThaisAlvesSilva <alvesthais406@gmail.com>
2024-06-06 15:58:53 -0300

    Corrigindo rotas

--- Remediar-FrontEnd/src/app/components/cadastro-de-doador/cadastro-de-doador.component.ts
+++ Remediar-FrontEnd/src/app/components/cadastro-de-doador/cadastro-de-doador.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-de-doador',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterOutlet, RouterLink],
   templateUrl: './cadastro-de-doador.component.html',
   styleUrl: './cadastro-de-doador.component.css'
 })
 export class CadastroDeDoadorComponent {
+
+  constructor(private formBuilder: FormBuilder, private router: Router){}
+
+  goBack(): void {
+    this.router.navigate(['/']); 
+  }
 
 }
