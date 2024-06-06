8671a6d3ea9dd362fac2650abb84c77ecdada471
ThaisAlvesSilva <alvesthais406@gmail.com>
2024-06-06 15:05:47 -0300

    Pedidos realizados

--- Remediar-FrontEnd/src/app/app.routes.ts
+++ Remediar-FrontEnd/src/app/app.routes.ts
@@ -46,7 +46,7 @@ export const routes: Routes = [
         component: HistoricoDoadoresComponent
     },
     {
-        path:'pedidos',
+        path:'PedidosRealizados',
         component: PedidosComponent
     }
 ];
