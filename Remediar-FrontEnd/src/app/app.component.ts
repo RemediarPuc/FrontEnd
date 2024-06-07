9e73385a886f97958dacdfb73763482a06a1b833
NicolasMatheusFerreira <nicolasmatheusferreira13@gmail.com>
2024-06-07 19:37:10 -0300

    Atualizando RelatoriosDeDoador

--- Remediar-FrontEnd/src/app/app.component.ts
+++ Remediar-FrontEnd/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { HistoricoDoadorComponent } from './historico-doador/historico-doador.component';
+import { RelatoriosDeDoadorComponent } from './relatorios-de-doador/relatorios-de-doador.component';
 
 @Component({
-  selector: 'app-historico-doador',
+  selector: 'app-relatorios-de-doador',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, HistoricoDoadorComponent],
-  templateUrl: '/historico-doador.component.html',
-  styleUrl: './historico-doador.component.css'
+  imports: [RouterLink, RouterOutlet, RelatoriosDeDoadorComponent],
+  templateUrl: './relatorios-de-doador.component.html',
+  styleUrl: './relatorios-de-doador.component.css'
 })
 export class AppComponent {
-  title = 'Histórico de doadores';
+  title = 'Relatorios de doadores';
 }
