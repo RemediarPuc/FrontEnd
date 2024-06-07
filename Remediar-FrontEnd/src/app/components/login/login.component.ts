a230fdc82309b6287103f801125010de32dd1554
ThaisAlvesSilva <alvesthais406@gmail.com>
2024-06-07 20:30:37 -0300

    Implementando Login

--- Remediar-FrontEnd/src/app/components/login/login.component.ts
+++ Remediar-FrontEnd/src/app/components/login/login.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/Usuario';
+import { TipoUsuario } from '../../models/TipoUsuario';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +18,7 @@ export class LoginComponent {
   loginForm!: FormGroup;
   sucessLogin:boolean = true;
 
-  constructor(private formBuilder: FormBuilder, private router: Router){}
+  constructor(private formBuilder: FormBuilder, private router: Router, private service:UsuarioService ){}
 
   ngOnInit(): void{
     this.loginForm = this.formBuilder.group({
@@ -27,9 +30,38 @@ export class LoginComponent {
   submit(){
     if(this.loginForm.valid){
       const {value} = this.loginForm;
+      this.service.getUsuarioLogin(value.email, value.senha)
+        .subscribe(
+          (retorno) => {
+            this.sucessLogin = true;
+            console.log(TipoUsuario[retorno.data.tipoUsuario]);
+            let objeto ={
+              Id: retorno.data.id,
+              nome: retorno.data.nome,
+              tipo: TipoUsuario[retorno.data.tipoUsuario]
+            }
+            this.adicionaDadosLocalStorage(objeto);
+            this.redirecionaUsuario(objeto);
+          },
+          (error) => {
+            this.sucessLogin = false;
+          }
+        );
     }else{
       return;
     }
   }
 
+  adicionaDadosLocalStorage(usuario:any){
+    const usuarioSerializado = JSON.stringify(usuario);
+    localStorage.setItem('Usuario', usuarioSerializado);
+  }
+
+  redirecionaUsuario(usuario:any){
+    if(usuario.tipo == 'Usuário'){
+      this.router.navigate(['/HomeDoador']);
+    }else{
+      this.router.navigate(['/HomeAdmin']);
+    }
+  }
 }
