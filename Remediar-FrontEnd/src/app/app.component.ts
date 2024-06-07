e0ad7f520b42d437e6d1285ad1a5740e8bc98120
ThaisAlvesSilva <alvesthais406@gmail.com>
2024-06-07 18:54:15 -0300

    criando service de exemplo

--- Remediar-FrontEnd/src/app/app.component.ts
+++ Remediar-FrontEnd/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import {CadastroDeDoadorComponent} from './components/cadastro-de-doador/cadastro-de-doador.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,FormsModule,ReactiveFormsModule, LoginComponent],
+  imports: [RouterOutlet,RouterLink,FormsModule,ReactiveFormsModule, LoginComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Remediar-FrontEnd/src/app/services/usuario.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment.module';
+import { Usuario } from '../models/Usuario';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http:HttpClient) { }
+
+  getUsuarioLogin(email:String, senha:String):Observable<any>{
+    const url = `${environment.baseUrlApi}/Clientes/login/${email}/${senha}`
+    return this.http.get<Usuario>(url, { responseType: 'json' });
+  }
+}
