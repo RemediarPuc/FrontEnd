54dfee0dbce3826ff10e61640fb098e10ddada54
NicolasMatheusFerreira <nicolasmatheusferreira13@gmail.com>
2024-06-07 19:22:33 -0300

    Adicionando relatorios de doador

---
+++ Remediar-FrontEnd/src/app/relatorios-de-doador/relatorios-de-doador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorios-de-doador',
+  standalone: true,
+  imports: [],
+  templateUrl: './relatorios-de-doador.component.html',
+  styleUrl: './relatorios-de-doador.component.css'
+})
+export class RelatoriosDeDoadorComponent {
+
+}
