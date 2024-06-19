083977ca90bf21a749d29a3e2b03dd4593ef1e37
ThaisAlvesSilva <alvesthais406@gmail.com>
2024-06-19 17:54:17 -0300

    corrigindo service usuario

--- Remediar-FrontEnd/src/app/services/usuario.service.ts
+++ Remediar-FrontEnd/src/app/services/usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient } from '@angular/common/http';
 import { Usuario } from '../models/Usuario';
 
 @Injectable({
@@ -8,11 +9,16 @@ import { Usuario } from '../models/Usuario';
 })
 export class UsuarioService {
 
-baseUrl = `${environment.baseUrlApi}/Usuario`;
+  baseUrl = `${environment.baseUrlApi}/Usuario`;
 
-constructor( private htppClient: HttpClient) { }
-   
-  cadastro(usuario : Usuario){
-    return this.htppClient.post(this.baseUrl,usuario)
+  constructor( private htppClient: HttpClient) { }
+
+  cadastro(usuario : Usuario):Observable<Usuario>{
+    return this.htppClient.post<Usuario>(this.baseUrl,usuario)
+  }
+
+  getUsuarioLogin(email:String, senha:String):Observable<any>{
+    const url = `${this.baseUrl}/login/${email}/${senha}`
+    return this.htppClient.get<Usuario>(url, { responseType: 'json' });
   }
 }
