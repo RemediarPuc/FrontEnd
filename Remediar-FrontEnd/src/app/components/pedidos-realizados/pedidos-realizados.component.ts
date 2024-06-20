23ec8574fd4f2c037bb7716f964f0e3713fef9b6
gabriellaxdantas <gabriellaxdantas@gmail.com>
2024-06-19 21:49:08 -0300

    Adicionando back

--- Remediar-FrontEnd/src/app/components/pedidos-realizados/pedidos-realizados.component.ts
+++ Remediar-FrontEnd/src/app/components/pedidos-realizados/pedidos-realizados.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, DatePipe, Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../header/header.component';
+import { PedidoService } from '../../services/pedido.service';
 
 @Component({
   selector: 'app-pedidos-realizados',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, CommonModule, FormsModule, HeaderComponent],
   templateUrl: './pedidos-realizados.component.html',
-  styleUrl: './pedidos-realizados.component.css'
+  styleUrls: ['./pedidos-realizados.component.css'],
+  providers: [PedidoService]
 })
-export class PedidosRealizadosComponent {
+export class PedidosRealizadosComponent implements OnInit {
+  public pedidos: any[] = [];
+  public doacoesFiltradas: any[] = [];
+  public filtroStatus: string = 'Todos';
 
+  constructor(private pedidoService: PedidoService, private location: Location) { }
+
+  ngOnInit(): void {
+    this.pedidoService.getAllPedidos()
+      .subscribe(
+        (retorno) => {
+          this.pedidos = retorno.data;
+          this.doacoesFiltradas = retorno.data;
+          this.filtrarDoacoes();
+        },
+        (error) => {
+          console.log("Erro: ", error);
+        }
+      );
+  }
+
+  filtrarDoacoes(): void {
+    if (this.pedidos != null) {
+      if (this.filtroStatus === 'Todos') {
+        this.doacoesFiltradas = this.pedidos;
+      } else {
+        this.doacoesFiltradas = this.pedidos.filter(x => x.statusDoacao === this.filtroStatus);
+      }
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
