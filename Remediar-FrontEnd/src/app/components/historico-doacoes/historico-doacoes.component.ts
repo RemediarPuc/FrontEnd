e6a1ced047597892a4b0e107a9e50c17738b6da8
ThaisAlvesSilva <alvesthais406@gmail.com>
2024-06-21 11:32:41 -0300

    Corrigindo historico de doações

--- Remediar-FrontEnd/src/app/components/historico-doacoes/historico-doacoes.component.ts
+++ Remediar-FrontEnd/src/app/components/historico-doacoes/historico-doacoes.component.ts
@@ -3,15 +3,17 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HistoricoDoacoesService } from '../../services/historico-doacoes.service';
-import { LocalStorageService } from '../../services/local-storage-service.service'; // Importar o serviço de Local Storage
+import { LocalStorageService } from '../../services/local-storage-service.service';
+import { VoltarComponent } from "../voltar/voltar.component";
+import { HeaderComponent } from "../header/header.component"; // Importar o serviço de Local Storage
 
 @Component({
-  selector: 'app-historico-doacoes',
-  standalone: true,
-  imports: [CommonModule, FormsModule, DatePipe],
-  templateUrl: './historico-doacoes.component.html',
-  styleUrls: ['./historico-doacoes.component.css'],
-  providers: [HistoricoDoacoesService]
+    selector: 'app-historico-doacoes',
+    standalone: true,
+    templateUrl: './historico-doacoes.component.html',
+    styleUrls: ['./historico-doacoes.component.css'],
+    providers: [HistoricoDoacoesService],
+    imports: [CommonModule, FormsModule, DatePipe, VoltarComponent, HeaderComponent]
 })
 export class HistoricoDoacoesComponent implements OnInit {
   public historicoDoacoes: any[] = [];
@@ -23,9 +25,9 @@ export class HistoricoDoacoesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.usuarioId = this.localStorageService.getUserId();
-    if (this.usuarioId !== null) {
-      this.historicoDoacoesService.getHistoricoDoacoesPorUsuario(this.usuarioId)
+    const usuario = this.localStorageService.getItem("Usuario");
+    if (usuario !== null) {
+      this.historicoDoacoesService.getHistoricoDoacoesPorUsuario(usuario.Id)
         .subscribe(
           (data) => {
             this.historicoDoacoes = data;
