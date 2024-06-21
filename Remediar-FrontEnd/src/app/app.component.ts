8ed310b82a5859ff335c2ab5d9ec42e976ce0ed8
gabriellaxdantas <gabriellaxdantas@gmail.com>
2024-06-20 22:59:24 -0300

    removendo erro

--- Remediar-FrontEnd/src/app/app.component.ts
+++ Remediar-FrontEnd/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SlickCarouselComponent } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-root',
